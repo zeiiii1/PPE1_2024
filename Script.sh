8be1875e40bb41dd825b7a4474de66d176ae0d6b
Zeinab Omar <zeinabomar@MacBook-Air-de-Zeinab.local>
2024-10-20 18:43:31 +0200

    erreur script corrigée

--- Script.sh
+++ Script.sh
@@ -1,6 +1,6 @@
  #!/usr/bin/bash
 
-chemin_ann="/Users/zeinabomar/Desktop/M1\ TAL/Programmation\ Projet\ Encadré/TD1/Exercice1/ann/"
+chemin_ann="/Users/zeinabomar/Desktop/M1\ TAL/Programmation\ Projet\ Encadré/TD1/Exercice1/ann"
 chemin_fichier="/Users/zeinabomar/Desktop/M1\ TAL/Programmation\ Projet\ Encadré/PPE1_2024/Exercice/comptes.sh"
 
 for annee in 2016 2017 2018 
