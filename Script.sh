150568ac42144f7be6609c4f675b631182ed2cdc
Zeinab Omar <zeinabomar@MacBook-Air-de-Zeinab.local>
2024-10-20 18:48:17 +0200

    version final

--- Script.sh
+++ Script.sh
@@ -1,10 +1,11 @@
- #!/usr/bin/bash
+#!/usr/bin/bash
 
-chemin_ann="/Users/zeinabomar/Desktop/M1\ TAL/Programmation\ Projet\ Encadré/TD1/Exercice1/ann"
-chemin_fichier="/Users/zeinabomar/Desktop/M1\ TAL/Programmation\ Projet\ Encadré/PPE1_2024/Exercice/comptes.sh"
+chemin_ann="/Users/zeinabomar/Desktop/M1 TAL/Programmation Projet Encadré/TD1/Exercice1/ann"
+chemin_fichier="/Users/zeinabomar/Desktop/M1 TAL/Programmation Projet Encadré/PPE1_2024/Exercice/comptes.sh"
 
-for annee in 2016 2017 2018 
-do 	
-	compte=$(grep "Location" "$chemin_ann/$annee/*/*.ann" | wc -l) 
-	echo -e "Annotations en $annee :\n$compte\n" >> $chemin_fichier
-done 
+for annee in 2016 2017 2018
+do
+    compte=$(grep "Location" "$chemin_ann/$annee"/*/*.ann | wc -l)
+    echo -e "Annotations en $annee :\n$compte\n" >> "$chemin_fichier"
+done
+ 
