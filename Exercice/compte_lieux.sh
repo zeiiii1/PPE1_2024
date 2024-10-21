e5829e92d1fae7d6e00301c8376043d18db0dcc7
Zeinab Omar <zeinabomar@MacBook-Air-de-Zeinab.local>
2024-10-21 11:45:52 +0200

    arguments validation ajouté

--- Exercice/compte_lieux.sh
+++ Exercice/compte_lieux.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 
+
+if [ "$#" -ne 3 ]; then
+    echo "Format arguments : <annee> <mois> <nombre_de_lieux>"
+    exit 1
+fi
+
+
 annee=$1
 mois=$2
 nombre_de_lieux=$3
 
+
 chemin="/Users/zeinabomar/Desktop/M1 TAL/Programmation Projet Encadré/TD1/Exercice1/ann/$annee/$mois"
 
-grep "Location" "$chemin"/*.ann | cut -f3 | sort | uniq -c | sort -nr | head -n "$nombre_de_lieux"
+compte=$(grep "Location" "$chemin"/*.ann | cut -f3 | sort | uniq -c | sort -nr | head -n "$nombre_de_lieux")
+echo "$compte"
 
