5eb175ee3b62a10b00030e44f18425c0f809d57d
Zeinab Omar <zeinabomar@MacBook-Air-de-Zeinab.local>
2024-10-21 12:06:09 +0200

    arguments validation ajouté

--- Exercice/compte_par_type_par_an.sh
+++ Exercice/compte_par_type_par_an.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/bash
-
+!/usr/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "Format argument : <type_entite>"                         
+  	exit 1                                                                      
+fi  
 chemin="/Users/zeinabomar/Desktop/M1 TAL/Programmation Projet Encadré/TD1/Exercice1/ann"
 
 type_entite=$1
 
-for annee in 2016 2017 2018 
-do 
+for annee in 2016 2017 2018
+do
 	compte=$(grep "$type_entite" "$chemin/$annee"/*/*.ann | wc -l)
 	echo -e "Annontations en $annee : \n$compte\n"
-done 
-
+done
