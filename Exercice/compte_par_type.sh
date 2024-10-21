fb731b512e33b671e36b270347940eab49c53611
Zeinab Omar <zeinabomar@MacBook-Air-de-Zeinab.local>
2024-10-21 11:48:54 +0200

    arguments validation ajouté

--- Exercice/compte_par_type.sh
+++ Exercice/compte_par_type.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-
+if [ "$#" -ne 2 ]; then
+	echo "Format arguments : <annee> <type_entite>"                  
+  	exit 1                                                                      
+fi                                                                              
 chemin="/Users/zeinabomar/Desktop/M1 TAL/Programmation Projet Encadré/TD1/Exercice1/ann"
 
 annee=$1
